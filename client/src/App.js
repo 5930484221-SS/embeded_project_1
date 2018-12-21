dd27ab6895e494921216b6fa3a59ea37f82b7b54
Sarn Sornpaisarn <fsornpaisarn@gmail.com>
2018-12-21 08:08:17 +0700

    final

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
   };
 
   componentDidMount() {
+    this.getCurrentState();
     this.response();
   }
 
@@ -27,6 +28,17 @@ class App extends Component {
       console.log("socket working fine...");
     });
   };
+
+  getCurrentState = () => {
+    axios.get(endpoint + '/api/sensor').then(res => {
+      const tmp = res.data;
+      this.setState({
+        bmp = tmp.bpm,
+        min_heartRate = tmp.min_bpm,
+        max_heartRate = tmp.max_bpm
+      });
+    })
+  }
 
   send = () => {
     const { min_heartRate, max_heartRate, endpoint } = this.state;
